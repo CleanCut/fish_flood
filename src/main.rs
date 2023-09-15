e53d030acd650c9f23c9f3df0ddbb0c6aa1d946a
Nathan Stocks <cleancut@github.com>
2023-09-15 13:39:53 -0600

    final state for the game jam

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,35 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(1000.0))
-        .add_plugins(RapierDebugRenderPlugin::default())
+        // .add_plugins(RapierDebugRenderPlugin::default())
         .insert_resource(ClearColor(Color::rgb_u8(176, 234, 238)))
         .add_systems(Startup, setup)
-        .add_systems(Update, (menu, spray, score))
+        .add_systems(Update, (menu, spray, score, cart_spawner))
+        .insert_resource(CartSpawner {
+            timer: Timer::from_seconds(5.0, TimerMode::Repeating),
+        })
         .run();
+}
+
+#[derive(Resource)]
+struct CartSpawner {
+    timer: Timer,
+}
+
+fn cart_spawner(
+    time: Res<Time>,
+    mut cart_spawner: ResMut<CartSpawner>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    if cart_spawner.timer.tick(time.delta()).just_finished() {
+        spawn_cart(&mut commands, &asset_server);
+    }
+}
+
+#[derive(Component)]
+struct Score {
+    score: usize,
 }
 
 #[derive(Component)]
@@ -52,34 +76,6 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         .insert(RigidBody::Fixed)
         .insert(TransformBundle::from(Transform::from_xyz(0.0, -337.0, 0.0)));
 
-    // Cart
-    commands
-        .spawn(RigidBody::KinematicVelocityBased)
-        .insert(SpriteBundle {
-            transform: Transform::from_translation(Vec3::new(-400.0, -241.0, 1.0)),
-            texture: asset_server.load("sprite/robot1.png"),
-            ..Default::default()
-        })
-        .insert(Velocity {
-            linvel: Vec2::new(100.0, 0.0),
-            ..Default::default()
-        })
-        .insert(Cart::default())
-        .with_children(|children| {
-            children.spawn(Collider::compound(vec![
-                (Vec2::new(-85.0, -2.0), 0.0, Collider::cuboid(5.0, 68.0)),
-                (Vec2::new(85.0, -2.0), 0.0, Collider::cuboid(5.0, 68.0)),
-                (Vec2::new(0.0, -60.0), 0.0, Collider::cuboid(85.0, 10.0)),
-            ]));
-            children
-                .spawn(Collider::compound(vec![(
-                    Vec2::new(0.0, 8.0),
-                    0.0,
-                    Collider::cuboid(80.0, 58.0),
-                )]))
-                .insert(Sensor);
-        });
-
     // Ground
     for i in 0..10 {
         commands.spawn(SpriteBundle {
@@ -99,6 +95,57 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         texture: asset_server.load("sprite/big-hose.png"),
         ..Default::default()
     });
+
+    // Score Text
+    let font = asset_server.load("FiraMono-Medium.ttf");
+    commands.spawn((
+        Text2dBundle {
+            text: Text::from_section(
+                "Score: 0",
+                TextStyle {
+                    font: font.clone(),
+                    font_size: 60.0,
+                    color: Color::BLACK,
+                },
+            ),
+            transform: Transform::from_translation(Vec3::new(-500.0, 320.0, 50.0)),
+            ..Default::default()
+        },
+        Score { score: 0 },
+    ));
+
+    spawn_cart(&mut commands, &asset_server);
+}
+
+fn spawn_cart(commands: &mut Commands, asset_server: &AssetServer) {
+    commands
+        .spawn(RigidBody::KinematicVelocityBased)
+        .insert(SpriteBundle {
+            transform: Transform::from_translation(Vec3::new(-650.0, -241.0, 1.0)),
+            texture: asset_server.load("sprite/robot1.png"),
+            ..Default::default()
+        })
+        .insert(Velocity {
+            linvel: Vec2::new(100.0, 0.0),
+            ..Default::default()
+        })
+        .insert(Cart {
+            capacity: 15,
+            count: 0,
+        })
+        .insert(Collider::compound(vec![(
+            Vec2::new(0.0, 8.0),
+            0.0,
+            Collider::cuboid(80.0, 58.0),
+        )]))
+        .insert(Sensor)
+        .with_children(|children| {
+            children.spawn(Collider::compound(vec![
+                (Vec2::new(-85.0, -2.0), 0.0, Collider::cuboid(5.0, 68.0)),
+                (Vec2::new(85.0, -2.0), 0.0, Collider::cuboid(5.0, 68.0)),
+                (Vec2::new(0.0, -60.0), 0.0, Collider::cuboid(85.0, 10.0)),
+            ]));
+        });
 }
 
 fn menu(keyboard_input: Res<Input<KeyCode>>, mut exit: EventWriter<AppExit>) {
@@ -165,4 +212,40 @@ fn spray(
     }
 }
 
-fn score() {}
+fn score(
+    rapier_context: Res<RapierContext>,
+    mut cart_query: Query<(Entity, &mut Cart, &Transform)>,
+    mut score_query: Query<(&mut Score, &mut Text)>,
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    let Ok((mut score, mut text)) = score_query.get_single_mut() else {
+        return;
+    };
+    for (entity, mut cart, transform) in cart_query.iter_mut() {
+        // How many fish are in the cart?
+        let cart_count = rapier_context.intersections_with(entity).count();
+        if cart_count > cart.capacity {
+            // Cart breaks?
+            commands.entity(entity).despawn_recursive();
+            if score.score > cart_count {
+                score.score -= cart_count;
+            } else {
+                score.score = 0;
+            }
+        } else if transform.translation.x > 750.0 {
+            // Cart scores?
+            commands.entity(entity).despawn_recursive();
+            score.score += cart_count;
+        }
+        text.sections[0].value = format!("Score: {}", score.score);
+        // for (collider1, collider2, intersecting) in rapier_context.intersections_with(entity) {
+        //     if intersecting {
+        //         println!(
+        //             "The entities {:?} and {:?} have intersecting colliders!",
+        //             collider1, collider2
+        //         );
+        //     }
+        // }
+    }
+}
